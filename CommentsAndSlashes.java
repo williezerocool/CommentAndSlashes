0b0cca226961938cbc729b9f1a9849388c0f16c3
Willie Suggs <klbooker09@gmail.com>
2017-05-05 01:42:12 -0500

    Add files via upload

---
+++ CommentsAndSlashes.java
@@ -0,0 +1,15 @@
+//Willie May 3, 2017
+//java comments are the same as javascript comments
+public class CommentsAndSlashes
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "I could have code like this." );
+
+        
+        // System.out.println("This won't run.");
+
+        System.out.println( "This will run." );
+    }
+}
